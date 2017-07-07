094881b6a5bbdf3a60281ec807e0261c30cc9458
William Wang <willwang343@gmail.com>
2017-07-07 15:00:44 -0400

    output file includes homology and num pts

--- computation.cpp
+++ computation.cpp
@@ -65,6 +65,7 @@ std::unique_ptr<ComputationResult> Computation::compute_raw(ComputationInput& in
 
     time = timer.elapsed();
     debug() << "  -- bifiltration computation took " << time << " milliseconds";
+    output << "homol" << params.dim << " " << "pts" << params.shortName.substr(13, 16) << " ";
     output << time << " ";
 
     //write Macaulay2 file for computing Betti numbers
