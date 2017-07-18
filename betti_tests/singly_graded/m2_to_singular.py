625083cf746a2d3e0c156e44cb957cf1dc82a0f5
William Wang <willwang343@gmail.com>
2017-07-17 21:40:43 -0400

    more scripts tweaking

--- betti_tests/singly_graded/m2_to_singular.py
+++ betti_tests/singly_graded/m2_to_singular.py
@@ -1,6 +1,5 @@
 # Use a singly-graded M2 file to produce a Singular file.
 # RUN THIS SCRIPT BEFORE THE M2 FILES ARE SIMPLIFIED
-# TODO: Fix issues with 0th homology
 
 import os
 import string
@@ -9,6 +8,7 @@ import re
 for file_name in os.listdir():
 	if not file_name.endswith("singly_graded.m2"):
 		continue
+	is_0th_homol = True if file_name[-18:-17] == '0' else False
 	res_file = file_name[:-3] + "_res.sing"
 	print("Writing the file %s" % res_file)
 
@@ -41,13 +41,14 @@ for file_name in os.listdir():
 		f1.write(",".join(c1_strings))
 		f1.write(";\n")
 	else:
+		print("Warning: Nothing contained in c1 of M2 file")
 		f1.write("intvec single_grades_C1;\n")
 
-	# Also collect C0
+	# Also collect C0. However, what we write depends on the homology.
 	c0 = f2.readline()
 	if c0[:6] != "C0=R^{" or c1[-2:] != "}\n":
 		print("Warning: unexpected format of M2 file on line 4")
-	if len(c0) != 8:
+	if not is_0th_homol:
 		c0_strings = c0[7:-3].split("},{")
 		# Remove the minus sign if the number is not 0
 		c0_strings = map(lambda x : x if x == "0" else x[1:], c0_strings)
@@ -55,7 +56,7 @@ for file_name in os.listdir():
 		f1.write(",".join(c1_strings))
 		f1.write("\n")
 	else:
-		f1.write("intvec single_grades_C0;\n")
+		f1.write("//No C0 for 0th homology\n")
 
 	# Line5 should be Id=promote(1,R).
 	if f2.readline() != "Id=promote(1,R)\n":
@@ -63,10 +64,10 @@ for file_name in os.listdir():
 
 	# Collect the data to make d2. We do this by extracting all the ordered pairs first, since 
 	# the columns of d2 are identified one at a time.
-	f1.write("module d2;\n")
 	d2 = f2.readline()
 	if d2[:14] != "d2=map(C1,C2,{" or d2[-4:] != "});\n":
 		print("Warning: unexpected format of M2 file on line 6")
+	f1.write("module d2;\n")
 	# d2_pieces contains things of the form (a,b)=>c, where a and b are integers and c is a monomial
 	d2_pieces = re.findall(r"\(.*?\)=>[^,]*", d2[14:-4])
 	d2_dict = dict()
@@ -94,54 +95,68 @@ for file_name in os.listdir():
 		terms = map(lambda x : "%s*gen(%d)" % (x[1], x[0]), pairs)
 		f1.write("%s;\n" % "+".join(terms))
 
-	# Repeat for d1
-	f1.write("module d1;\n")
+	# Repeat for d1. Once again, this is irrelevant for 0th homology.
 	d1 = f2.readline()
 	if d1[:14] != "d1=map(C0,C1,{" or d1[-4:] != "});\n":
 		print("Warning: unexpected format of M2 file on line 7")
 	# d2_pieces contains things of the form (a,b)=>c, where a and b are integers and c is a monomial
-	d1_pieces = re.findall(r"\(.*?\)=>[^,]*", d1[14:-4])
-	d1_dict = dict()
-	for term in d1_pieces:
-		comma_pos = term.find(",")
-		if comma_pos == -1:
-			print("Warning: string matching failed for line 7")
-		x_val = int(term[1 : comma_pos])
-		arrow_pos = term.find(")=>")
-		if arrow_pos == -1:
-			print("Warning: string matching failed for line 7")
-		y_val = int(term[comma_pos + 1 : arrow_pos])
-		monomial = term[arrow_pos + 3:]
-		# need to put the monomial (monomial) in the correct position (x_val, y_val).
-		# In general, we use d[y_val] = stuff with x_val.
-		# We store the items in a dictionary: keys are y coords, items are lists of (x, monomial) tuples
-		if y_val in d1_dict:
-			d1_dict.get(y_val).append((x_val, monomial))
-		else:
-			d1_dict[y_val] = [(x_val, monomial)]
-	# Data collected; now, output it to the file.
-	for key in d1_dict:
-		f1.write("d1[%d]=" % key)
-		pairs = d1_dict[key]
-		terms = map(lambda x : "%s*gen(%d)" % (x[1], x[0] + 1), pairs)
-		f1.write("+".join(terms))
+	if not is_0th_homol:
+		f1.write("module d1;\n")
+		d1_pieces = re.findall(r"\(.*?\)=>[^,]*", d1[14:-4])
+		d1_dict = dict()
+		for term in d1_pieces:
+			comma_pos = term.find(",")
+			if comma_pos == -1:
+				print("Warning: string matching failed for line 7")
+			x_val = int(term[1 : comma_pos])
+			arrow_pos = term.find(")=>")
+			if arrow_pos == -1:
+				print("Warning: string matching failed for line 7")
+			y_val = int(term[comma_pos + 1 : arrow_pos])
+			monomial = term[arrow_pos + 3:]
+			# need to put the monomial (monomial) in the correct position (x_val, y_val).
+			# In general, we use d[y_val] = stuff with x_val.
+			# We store the items in a dictionary: keys are y coords, items are lists of (x, monomial) tuples
+			if y_val in d1_dict:
+				d1_dict.get(y_val).append((x_val, monomial))
+			else:
+				d1_dict[y_val] = [(x_val, monomial)]
+		# Data collected; now, output it to the file.
+		for key in d1_dict:
+			f1.write("d1[%d]=" % key)
+			pairs = d1_dict[key]
+			terms = map(lambda x : "%s*gen(%d)" % (x[1], x[0] + 1), pairs)
+			f1.write("+".join(terms))
+	else:
+		f1.write("//No d1 for 0th homology\n")
 
 
 	# Almost done! We don't care about the rest of the M2 file (which doesn't contain information we need).
 	f2.close()
-	f1.write('attrib(d2,"isHomog",single_grades_C1);\n')
-	f1.write('attrib(d1,"isHomog",single_grades_C0);\n')
-	f1.write('timer=0;\n')
-	f1.write('system("--ticks-per-sec",1000); //timer resolution in ms\n')
-	f1.write('int t_0=timer;\n')
-	f1.write('module triv=0;\n')
-	f1.write('def H=homology(d2,d1,triv,triv);\n')
-	f1.write('write(":a singular_res_times.txt", timer - t_0);\n')
-	f1.write('resolution resH=res(H,0);\n')	
-	f1.write('write(":a singular_res_times.txt", timer - t_0);\n')
-	f1.write('print(betti(resH), "betti");\n')
-	f1.write('write(":a singular_res_times.txt", timer - t_0);\n')
-	f1.write('write(":a singular_res_times.txt", "\\n");\n')
+	if is_0th_homol:
+		f1.write('attrib(d2,"isHomog",single_grades_C1);\n')
+		f1.write('timer=0;\n')
+		f1.write('system("--ticks-per-sec",1000); //timer resolution in ms\n')
+		f1.write('int t_0=timer;\n')
+		f1.write('resolution resH=res(d2,0);\n')	
+		f1.write('write(":a singular_res_times.txt", timer - t_0);\n')
+		f1.write('print(betti(resH), "betti");\n')
+		f1.write('write(":a singular_res_times.txt", timer - t_0);\n')
+		f1.write('write(":a singular_res_times.txt", "\\n");\n')
+	else:
+		f1.write('attrib(d2,"isHomog",single_grades_C1);\n')
+		f1.write('attrib(d1,"isHomog",single_grades_C0);\n')
+		f1.write('timer=0;\n')
+		f1.write('system("--ticks-per-sec",1000); //timer resolution in ms\n')
+		f1.write('int t_0=timer;\n')
+		f1.write('module triv=0;\n')
+		f1.write('def H=homology(d2,d1,triv,triv);\n')
+		f1.write('write(":a singular_res_times.txt", timer - t_0);\n')
+		f1.write('resolution resH=res(H,0);\n')	
+		f1.write('write(":a singular_res_times.txt", timer - t_0);\n')
+		f1.write('print(betti(resH), "betti");\n')
+		f1.write('write(":a singular_res_times.txt", timer - t_0);\n')
+		f1.write('write(":a singular_res_times.txt", "\\n");\n')
 	f1.close()
 
 	# Copy the lines into another file, with a couple of tweaks -- repeat with sres()
@@ -151,7 +166,7 @@ for file_name in os.listdir():
 
 	f1.write('LIB "schreyer.lib";\n')
 	for line in f2:
-		f1.write(line.replace("resolution resH=res(H,0);", "resolution resH=s_res(H,0);")
+		f1.write(line.replace("resolution resH=res(", "resolution resH=s_res(")
 			.replace("singular_res_times.txt", "singular_sres_times.txt"))
 	f1.close()
 	f2.close()
